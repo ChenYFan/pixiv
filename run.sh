4e37f8c492222b1d8a15eea16ba9b02a4bfa6d0e
ChenYFan <53730587+ChenYFan@users.noreply.github.com>
2020-07-31 09:19:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ chmod +x parse.sh
 
 cp -rf tpl/template.html src/index.html
 cp -rf tpl/CNAME src/CNAME
+
 curl -s "https://api.imjad.cn/pixiv/v2/?type=rank&mode=day&page=1" > tmp/pixiv.json
 ./parse.sh
 curl -s "https://api.imjad.cn/pixiv/v2/?type=rank&mode=day&page=2" > tmp/pixiv.json
@@ -24,7 +25,7 @@ cat tmp/main.html | tr "\n" " " > tmp/main-tpl.html
 pixivHTML=$(cat tmp/main-tpl.html)
 
 sed -i "s~{{Main}}~${pixivHTML}~g" src/index.html
-sed -i "s~i.pximg.net~i.pixiv.cat~g" src/index.html
+sed -i "s~i.pximg.net~pixiv-img.cyfan.top~g" src/index.html
 
 cp -rf src/index.html dist/index.html
 cp -rf src/CNAME dist/CNAME
